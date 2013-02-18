c311ddc1ccf2a7050293d8796ec8161e2355b94a
JesusSayes <jesussayes@gmail.com>
2013-02-18 16:07:47 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersuadirPe::Application.config.secret_token = '0c894ea4fb678b14ed49c713c951000a3b9efdf4068ef50037a6d48f1ab780b3c85393a0baed4eb959acf871307afad3ca95f70264f300055b8d572e499c7037'
