d3a1b3425c5937bec8d7d55cfbd7b2d7dc3a61e2
JesusSayes <jesussayes@gmail.com>
2013-03-07 14:23:45 -0500

    rvmrc - deploy 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,59 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "persuadir-pe"
+set :repository,  "git@github.com:JesusSayes/persuadir-pe.git"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_via, :remote_cache
+set :branch, "master"
+set :ssh_options, { :forward_agent => true }
+set :user, "ccastillo"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+server "www.cristiancastillo.com", :web, :app, :db, primary: true
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :rvm_ruby_string, '1.9.3'
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+require "bundler/capistrano"
+require 'capistrano-unicorn'
+require "rvm/capistrano"
+
+before 'deploy:setup', 'rvm:install_rvm'   # install RVM
+before 'deploy:setup', 'rvm:install_ruby'
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+after "deploy:setup", "deploy:setup_config"
+after "deploy:finalize_update", "deploy:symlink_config"
+before "deploy", "deploy:check_revision"
+
+namespace :deploy do
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    run "mkdir -p #{shared_path}/config"
+    run "mkdir -p #{shared_path}/db"
+    # run "touch #{shared_path}/db/production.sqlite3"
+    # run "touch #{shared_path}/db/production.sqlite3"
+    put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+    sudo "service nginx restart"
+  end
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
+  end
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+
+end
